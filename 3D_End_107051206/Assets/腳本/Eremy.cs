6ac87934a23eb0869c136204180b937a728f7653
xiaobu920 <71679970+xiaobu920@users.noreply.github.com>
2021-01-13 06:07:56 +0800

    死亡BUG

--- 3D_End_107051206/Assets/腳本/Eremy.cs
+++ 3D_End_107051206/Assets/腳本/Eremy.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using UnityEngine.AI;   //引用 人工智慧 API
+using UnityEngine.AI;
 
 public class Eremy : MonoBehaviour
 {
@@ -9,6 +9,11 @@ public class Eremy : MonoBehaviour
     public float stopDistance = 1.3f;
     [Header("攻擊冷卻時間"), Range(0, 50)]
     public float AttackCD = 2f;
+    [Header("攻擊中心點")]
+    public Transform atkPoint;
+    [Header("攻擊長度"), Range(0f, 5f)]
+    public float atkLength;
+
 
     private Transform player;
     private NavMeshAgent nav;
@@ -21,13 +26,9 @@ public class Eremy : MonoBehaviour
 
     private void Awake()
     {
-        //取得身上的元件<代理器>
         nav = GetComponent<NavMeshAgent>();
         ani = GetComponent<Animator>();
-
-        //尋找其他遊戲物件("物件名稱").變形元件
         player = GameObject.Find("Yong").transform;
-        //代理器 的 速度 與 停止距離
         nav.speed = speed;
         nav.stoppingDistance = stopDistance;
     }
@@ -37,17 +38,25 @@ public class Eremy : MonoBehaviour
         Track();
         Attack();
     }
+    /// <summary>
+    /// 繪製圖示事件
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(atkPoint.position, atkPoint.forward * atkLength);
+    }
+    //射線擊中的物件
+    private RaycastHit hit;
+
+
 
     /// <summary>
     /// 追蹤
     /// </summary>
     private void Track()
     {
-        //代理器.設定目的地(玩家的座標)
-        nav.SetDestination(player.position);
-
-        print("剩餘的距離：" + nav.remainingDistance);
-        //動畫控制器.設定布林值("參數名稱", 剩餘距離 > 停止距離)
+        nav.SetDestination(player.position);        
         ani.SetBool("戰鬥模式", nav.remainingDistance > stopDistance);
     }
 
@@ -58,22 +67,22 @@ public class Eremy : MonoBehaviour
     {
         if (nav.remainingDistance < stopDistance)
         {
-            //時間 累加 (一幀的時間)
             timer += Time.deltaTime;
 
-            //取得玩家的座標
             Vector3 pos = player.position;
-            //將玩家的座標Y軸 指定為 本物件的Y軸
             pos.y = transform.position.y;
-            //看向(修改過的玩家座標)
             transform.LookAt(pos);
 
-            //如果計時器 >= 冷卻時間 就攻擊 並且計時器歸零 (循環)
             if (timer >= AttackCD)
             {
                 ani.SetTrigger("攻擊觸發");
                 timer = 0;
+                if (Physics.Raycast(atkPoint.position, atkPoint.forward, out hit, atkLength, 1 << 8))
+                {
+                     hit.collider.GetComponent<Player>().Damage();
+                }
             }
+            
         }
     }
 
---
+++ 3D_End_107051206/Assets/腳本/Player.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public void Damage()
+    {
+        print("玩家受傷");
+    }
+}
