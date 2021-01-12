1b0020fa85dfa62e54b7367315e3cbd7477f41cf
xiaobu920 <71679970+xiaobu920@users.noreply.github.com>
2021-01-12 17:06:38 +0800

    123

---
+++ 3D_End_107051206/Assets/腳本/Eremy.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.AI;   //引用 人工智慧 API
+
+public class Eremy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 3;
+    [Header("停止距離"), Range(0, 50)]
+    public float stopDistance = 1.3f;
+    [Header("攻擊冷卻時間"), Range(0, 50)]
+    public float AttackCD = 2f;
+
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator ani;
+
+    /// <summary>
+    /// 計時器
+    /// </summary>
+    private float timer;
+
+    private void Awake()
+    {
+        //取得身上的元件<代理器>
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+
+        //尋找其他遊戲物件("物件名稱").變形元件
+        player = GameObject.Find("Yong").transform;
+        //代理器 的 速度 與 停止距離
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+    }
+
+    private void Update()
+    {
+        Track();
+        Attack();
+    }
+
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    private void Track()
+    {
+        //代理器.設定目的地(玩家的座標)
+        nav.SetDestination(player.position);
+
+        print("剩餘的距離：" + nav.remainingDistance);
+        //動畫控制器.設定布林值("參數名稱", 剩餘距離 > 停止距離)
+        ani.SetBool("戰鬥模式", nav.remainingDistance > stopDistance);
+    }
+
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    private void Attack()
+    {
+        if (nav.remainingDistance < stopDistance)
+        {
+            //時間 累加 (一幀的時間)
+            timer += Time.deltaTime;
+
+            //取得玩家的座標
+            Vector3 pos = player.position;
+            //將玩家的座標Y軸 指定為 本物件的Y軸
+            pos.y = transform.position.y;
+            //看向(修改過的玩家座標)
+            transform.LookAt(pos);
+
+            //如果計時器 >= 冷卻時間 就攻擊 並且計時器歸零 (循環)
+            if (timer >= AttackCD)
+            {
+                ani.SetTrigger("攻擊觸發");
+                timer = 0;
+            }
+        }
+    }
+
+
+
+
+}
---
+++ 3D_End_107051206/Assets/腳本/NPC.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;       //引用 系統.集合 API (包含協同程序)
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.2f;
+
+    /// <summary>
+    /// 玩家是否進入感應區
+    /// </summary>
+    public bool playerInArea;
+
+    // 定義列舉 eunm (下拉式選單 - 只能選一個)
+    public enum NPCSate
+    {
+        第一段對話, 任務進行中, 完成任務
+    }
+
+    //列舉欄位
+    //修飾詞 列舉名稱 自訂欄位名稱 指定 預設值;
+    public NPCSate state = NPCSate.第一段對話;   // if後面不加,預設值為第一個.
+
+
+    /* 協同程序
+    private void Start()
+    {
+        // 啟動協同程序
+        StartCoroutine(Test());
+    }
+
+    private IEnumerator Test()
+    {
+         print("");
+         yield return new WaitForSeconds(1.5f);
+        print("1.5秒後");
+        yield return new WaitForSeconds(2f);
+        print("2秒後");
+    }
+    */
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Yong")
+        {
+            playerInArea = true;
+            StartCoroutine(Dialog());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Yong")
+        {
+            playerInArea = false;
+            Stopdialog();
+        }
+    }
+
+    /// <summary>
+    /// 停止對話
+    /// </summary>
+    private void Stopdialog()
+    {
+        dialog.SetActive(false);    //關閉對話框
+        StopAllCoroutines();        //停止所有協程
+    }
+
+    /// <summary>
+    /// 開始對話
+    /// </summary>
+    private IEnumerator Dialog()
+    {
+        /**
+        //print(data.dialogA);       // 取得字串全部資料
+        //print(data.dialogA[3]);    //取得字串局部資料:語法[編號]
+
+        for 迴圈:重複處理相同程式
+        for (int i = 0; i < 10; i++)
+        {
+                print("我是迴圈：" + i);
+        }
+        
+        for (int apple = 1; apple < 100; apple++)
+        {
+            print("迴圈：" + apple);
+        }
+        */
+
+        //顯示對話框
+        dialog.SetActive(true);
+        // 清空文字
+        textContent.text = "";
+        // 對話者名稱 指定為 此物件的名稱
+        textName.text = name;
+
+        //要說的對話
+        string dialogString = data.dialogA;
+
+        switch (state)
+        {
+            case NPCSate.第一段對話:
+                dialogString = data.dialogA;
+                break;
+            case NPCSate.任務進行中:
+                dialogString = data.dialogB;
+                break;
+            case NPCSate.完成任務:
+                dialogString = data.dialogC;
+                break;
+        }
+
+        // 字串的長度 dialogA.Length
+        for (int i = 0; i < dialogString.Length; i++)
+        {
+            // print(data.dialogA[i]);
+            // 文字 串聯
+            textContent.text += dialogString[i] + "";
+            yield return new WaitForSeconds(interval);
+        }
+
+    }
+}
