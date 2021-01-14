9fa8007de95d3d78a4be761085f12036c7462949
xiaobu920 <71679970+xiaobu920@users.noreply.github.com>
2021-01-15 01:30:43 +0800

    鎖定

--- 3D_End_107051206/Assets/腳本/Player.cs
+++ 3D_End_107051206/Assets/腳本/Player.cs
@@ -1,9 +1,35 @@
 ﻿using UnityEngine;
+using Invector.vCharacterController;
 
 public class Player : MonoBehaviour
 {
+    private float HP = 100;
+    private Animator ani;
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+
+    //受傷
     public void Damage()
     {
-        print("玩家受傷");
+        HP -= 55;
+        ani.SetTrigger("受傷觸發");
+
+        if (HP <= 0)
+        {
+            Dead();
+        }
+    }
+
+    private void Dead()
+    {
+        ani.SetTrigger("死亡觸發");
+        vThirdPersonController vt = GetComponent<vThirdPersonController>();
+        vt.lockMovement = true;
+        vt.lockRotation = true;
+        
+
     }
 }
